b4cace0422337cd390600a3cc89c38d7b4e0195c
kinting <kinting0130@gmail.com>
2019-05-16 23:36:49 +0800

    FizzBuzz project

---
+++ test/search_spec.js
@@ -0,0 +1,20 @@
+var should = chai.should()
+
+describe('FizzBuzz test', function () {
+  it('[case1]: input: 9, expect output : Fizz', function () {
+    const result = fizzBuzz(9)
+    result.should.be.equal('Fizz')
+  })
+  it('[case2]:input:10,ecpect output: Buzz', function () {
+    const result = fizzBuzz(10)
+    result.should.be.equal('Buzz')
+  })
+  it('[case3]:input:15,ecpect output:FizzBuzz', function () {
+    const result = fizzBuzz(15)
+    result.should.be.equal('FizzBuzz')
+  })
+  it('[case4]:input:7,ecpect output:7', function () {
+    const result = fizzBuzz(7)
+    result.should.be.equal(7)
+  })
+})
